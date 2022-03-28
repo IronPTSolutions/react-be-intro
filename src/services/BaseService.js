cf129dc2e3234e8330e85c3e02e86215b1c03440
Carlos de la Torre Díaz <carlosdelatorrediaz@gmail.com>
2022-03-28 21:22:46 +0200

    Update BaseService.js

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -7,56 +7,7 @@ const http = axios.create({
 
 http.interceptors.response.use(
   response => response.data,
-  error => new Promise.reject(error)
+  error => Promise.reject(error)
 )
 
 export default http
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from 'axios'
-
-// const http = axios.create({
-//   baseURL: "https://jsonplaceholder.typicode.com",
-//   withCredentials: true
-// })
-
-// http.interceptors.response.use(
-//   response => response.data,
-//   error => {
-//     return Promise.reject(error)
-//   }
-// )
